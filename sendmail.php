92dec4f7f1482fbeb143f7b0c0a1e696257c9cbe
jiaoyf16 <76833534+jiaoyf16@users.noreply.github.com>
2023-04-29 22:55:44 +0800

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -1,27 +1,36 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Get the form fields
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $institution = $_POST["institution"];
-    $use = $_POST["use"];
-    $github = $_POST["github"];
 
-    // Build the email message
-    $to = "jiaoyf.thu@gmail.com"; // Replace with your email address
-    $subject = "SPHSOL Access Request";
-    $message = "Name: $name\n\nEmail: $email\n\nInstitution: $institution\n\nIntended use of SPHSOL: $use\n\nGitHub username: $github\n";
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Get form data
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $institution = $_POST['institution'];
+    $use = $_POST['use'];
+    $github = $_POST['github'];
 
-    // Set the email headers
+    // Set recipient email address
+    $to = 'jiaoyf.thu@gmail.com';
+
+    // Set email subject
+    $subject = 'SPHSOL Access Request';
+
+    // Build email body
+    $body = "Name: $name\n";
+    $body .= "Email: $email\n";
+    $body .= "Institution: $institution\n";
+    $body .= "Intended Use: $use\n";
+    $body .= "GitHub Username: $github\n";
+
+    // Set email headers
     $headers = "From: $email\r\n";
     $headers .= "Reply-To: $email\r\n";
-    $headers .= "CC: $email\r\n"; // Optional: send a copy of the email to the sender
+    $headers .= "X-Mailer: PHP/" . phpversion();
 
-    // Send the email
-    if (mail($to, $subject, $message, $headers)) {
-        echo "<p>Your email was sent successfully!</p>";
+    // Send email
+    if (mail($to, $subject, $body, $headers)) {
+        echo '<p>Your request has been sent. Thank you!</p>';
     } else {
-        echo "<p>There was an error sending your email. Please try again later.</p>";
+        echo '<p>There was an error sending your request. Please try again later.</p>';
     }
 }
 ?>
